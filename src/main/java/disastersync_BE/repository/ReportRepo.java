4c5524f4c2694f4c1bed4cd04a6139f738db59c0
KingNelx <tapiajonel606@gmail.com>
2025-04-17 15:31:41 +0800

    reset

--- src/main/java/disastersync_BE/repository/ReportRepo.java
+++
@@ -1,9 +0,0 @@
-package disastersync_BE.repository;
-
-import disastersync_BE.entity.Report;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReportRepo extends MongoRepository <Report, String> {
-}
